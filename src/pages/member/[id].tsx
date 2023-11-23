dd5f72ed84e81600a272b4527252b16af4dd1dc0
Jose Caldas <jrcaldas@yahoo.com.br>
2023-11-23 17:45:04 -0300

    fix undefined props

--- src/pages/member/[id].tsx
+++ src/pages/member/[id].tsx
@@ -23,36 +23,38 @@ export default function Member({ member }: { member: { params: User } }) {
           <S.MemberProfile>
             <S.MemberInfo>
               <div className="member-info-name">
-                <h2>{member.params.name.first}</h2>
-                <h2>{member.params.name.last}</h2>
+                <h2>{member.params?.name?.first}</h2>
+                <h2>{member.params?.name?.last}</h2>
               </div>
               <h3>Membro</h3>
               <img
-                src={member.params.picture.large}
-                alt={member.params.name.first}
+                src={member.params?.picture?.large}
+                alt={member.params?.name?.first}
               />
 
-              <h3>IDADE: {member.params.dob.age}</h3>
-              <h3>CEL: {member.params.cell}</h3>
+              <h3>IDADE: {member.params?.dob?.age}</h3>
+              <h3>CEL: {member.params?.cell}</h3>
             </S.MemberInfo>
             <S.MemberLocation>
-              <h2>Nome: {member.params.name.first}</h2>
-              <h2>Sobrenome: {member.params.name.last}</h2>
-              <h2>Gênero: {member.params.gender}</h2>
+              <h2>Nome: {member.params?.name?.first}</h2>
+              <h2>Sobrenome: {member.params?.name?.last}</h2>
+              <h2>Gênero: {member.params?.gender}</h2>
               <h4>
                 <Email size={20} color="#0070f3" /> Contato
               </h4>
+
+
               <h2>
                 Email:
-                <span> {member.params.email}</span>
+                <span> {member.params?.email}</span>
               </h2>
-              <h2>Telefone: {member.params.phone}</h2>
+              <h2>Telefone: {member.params?.phone}</h2>
               <h4>
                 <Location size={20} color="#0070f3" /> Localização
               </h4>
-              <p>Rua: {member.params.location.street}</p>
-              <p>Cidade: {member.params.location.city}</p>
-              <p>Estado: {member.params.location.state}</p>
+              <p>Rua: {member.params?.location.street}</p>
+              <p>Cidade: {member.params?.location.city}</p>
+              <p>Estado: {member.params?.location.state}</p>
               <S.Back>
                 <Link href="/">
                   Retornar à lista
@@ -76,7 +78,7 @@ export async function getStaticPaths() {
     const res = await axios.get<{ results: User[] }>(API_BASE_URL);
     const { results } = res.data;
     const members = results.map((member) => ({
-      params: { ...member, id: `${member.name.first}-${member.name.last}` },
+      params: { ...member, id: `${member?.name?.first}-${member?.name?.last}` },
     }));
 
     return {
@@ -101,7 +103,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { results } = res.data;
     const members = results.map((member) => ({
-      params: { ...member, id: `${member.name.first}-${member.name.last}` },
+      params: { ...member, id: `${member?.name?.first}-${member?.name?.last}` },
     }));
     return {
       props: {
