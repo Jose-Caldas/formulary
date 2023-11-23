c9b8d0ff2b1d6028f91ae9e7f30f9e875b43ba0d
Jose Caldas <jrcaldas@yahoo.com.br>
2023-11-23 17:05:09 -0300

    add ts ignore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <title>Desafio Juntos Somos Mais Front-end</title>
         </Head>
-        {/* @ts-expect-error this exists */}
         <Component {...pageProps} />
         <GlobalStyle />
       </UserProvider>
